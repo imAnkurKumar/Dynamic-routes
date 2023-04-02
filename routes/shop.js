dec30e770b4837745363a43ce53108493eadcc11
imAnkurKumar <imankur.ak@gmail.com>
2023-04-02 13:35:39 +0530

    modified

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,9 @@
 const path = require("path");
+
 const express = require("express");
+
 const shopController = require("../controllers/shop");
+
 const router = express.Router();
 
 router.get("/", shopController.getIndex);
@@ -12,6 +15,8 @@ router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", shopController.getCart);
 
 router.post("/cart", shopController.postCart);
+
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
 router.get("/orders", shopController.getOrders);
 
