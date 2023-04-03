d8574982757198165916a23c71347fcfa459b80f
imAnkurKumar <imankur.ak@gmail.com>
2023-04-03 16:24:47 +0530

    added mysql

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-db.execute("");
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
